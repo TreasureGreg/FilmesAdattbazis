de5434ce48282a65cfe23e3cbc4bb04b0b46e75b
TreasureGreg <gergo.kincses99@gmail.com>
2025-04-29 19:36:37 +0200

    neptun code

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <div class="mt-5"></div>
 
 <footer class="bg-light text-center text-muted py-3 border-top mt-auto">
-  <small>&copy; <?= date('Y') ?> Filmes adatbázis</small>
+  <small>&copy; <?= date('Y') ?> Filmes adatbázis - Kincses Gergő - DX7U5K</small>
 </footer>
 </body>
 </html>
